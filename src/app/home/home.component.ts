68d23769e14673308c2b0e6e3c4c824d86c67462
Henry <henrysoverholt@gmail.com>
2020-07-01 16:12:19 -0400

    changed home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DraftService } from "./../services/draft.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  players: any[] = this.draftService.allPlayers;
+  fieldGoalPlayers: any[] = [];
+  topFiveFieldPlayers: any[];
+  threePointPlayers: any[] = [];
+  topFiveThreePointPlayers: any[] = [];
+  constructor(private draftService: DraftService) {}
 
   ngOnInit() {
+    this.getFieldGoal();
+    this.getThreePoint();
   }
-
+  getFieldGoal() {
+    this.fieldGoalPlayers = this.players.sort((a, b) =>
+      a.fieldGoal < b.fieldGoal ? 1 : -1
+    );
+    this.topFiveFieldPlayers = [
+      this.fieldGoalPlayers[0],
+      this.fieldGoalPlayers[1],
+      this.fieldGoalPlayers[2],
+      this.fieldGoalPlayers[3],
+      this.fieldGoalPlayers[4],
+    ];
+    console.log(this.topFiveFieldPlayers);
+  }
+  getThreePoint() {
+    this.threePointPlayers = this.players.sort((a, b) =>
+      a.threePoint < b.threePoint ? 1 : -1
+    );
+    this.topFiveThreePointPlayers = [
+      this.threePointPlayers[0],
+      this.threePointPlayers[1],
+      this.threePointPlayers[2],
+      this.threePointPlayers[3],
+      this.threePointPlayers[4],
+    ];
+    console.log(this.topFiveThreePointPlayers);
+  }
+  goToLink(url: string) {
+    window.open(url, "_blank");
+  }
 }
